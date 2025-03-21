afb5796ebb87f4f2bac07103a0f466ac5b50563a
Tihomir Stanoev <tstanoev@yandex.com>
2025-03-21 23:30:09 +0200

    04. Extract Emails

--- 110_regular_expressions_exercise/4_extract_emails.py
+++ 110_regular_expressions_exercise/4_extract_emails.py
@@ -1,19 +1,9 @@
 import re
 
 
-class Email:
-    def __init__(self, text):
-        self.text = text
+new_string = input()
+regex = r'\b^[a-zA-Z0-9][0-9a-zA-Z-._]+@[\w\d][\w\d._-]+\.[\w]+\b'
+emails = re.finditer(regex, new_string)
 
-    def extract_emails(self):
-        regex = r'\s([a-z0-9][a-z0-9\-\.\_]+[a-z0-9]@[a-z]+[a-z0-9\-]+[a-z](\.[a-z]+)+)'
-        emails = re.finditer(regex, self.text)
-
-        return '\n'.join(email.group(1) for email in emails)
-
-
-single_string = input()
-
-addresses = Email(single_string)
-
-print(addresses.extract_emails())
+for email in emails:
+    print(email.groups())
